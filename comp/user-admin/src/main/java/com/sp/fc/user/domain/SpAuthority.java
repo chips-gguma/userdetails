1d93f39e1ffb9941c1b0ce1a0fe82d00cd0c80d8
chips-gguma <ljygguma@gmail.com>
2022-01-08 23:51:40 +0900

    SpAuthority 클래스 table로 지정

--- comp/user-admin/src/main/java/com/sp/fc/user/domain/SpAuthority.java
+++ comp/user-admin/src/main/java/com/sp/fc/user/domain/SpAuthority.java
@@ -8,15 +8,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity // db에 저장해야 함
+@Table(name = "sp_user_authority")
+@IdClass(SpAuthority.class)
 public class SpAuthority implements GrantedAuthority {
 
+    @Id
     private Long userId;
+
+    @Id
     private String authority;
 
 }
